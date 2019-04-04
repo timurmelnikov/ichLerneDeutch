1ea49c681bc41abe781a2f61b4631d50d7fcf84b
Timur Melnikov <melnikovt@gmail.com>
2019-04-04 13:34:03 +0300

    Базовые настройки проекта 18:20

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,33 @@
+<template>
+    <v-app>
+        <v-toolbar app dark class="primary">
+            <v-toolbar-title v-text="'Ich Learn Deutsh'"></v-toolbar-title>
+            <v-btn flat :href="linkLesson">{{Math.floor(timeLesson / 60).toString()+':' + (timeLesson % 60).toString()}}</v-btn>
+            <v-spacer></v-spacer>
+            <v-toolbar-items>
+                <v-btn flat>Button 1</v-btn>
+            </v-toolbar-items>
+        </v-toolbar>
+    </v-app>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                timeLesson: '1100',
+
+            }
+        },
+        computed: {
+            linkLesson (){
+                let link = 'https://www.youtube.com/watch?v=xodyM-xXrl4&list=PLu_62Q68DvTqmSjoY-3uPtJAXmT5WWMy1&index=2&t='
+                return link + this.timeLesson+'s'
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
